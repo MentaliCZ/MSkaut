6de45bd8350cdf4669d2983c718f63eeb2f1a601
Ondrej Litschmann <xlitschm@fi.muni.cz>
2025-04-13 20:41:40 +0200

    work on Transaction class

--- MSkaut/Transaction.cs
+++ MSkaut/Transaction.cs
@@ -2,7 +2,14 @@
 {
     public class Transaction
     {
+        private int id = -1;
+        public uint Amount { get; private set; }
+        public Date Date { get; private set; }
 
-
+        public Transaction(uint amount, Date date)
+        {
+            this.Amount = amount;
+            this.Date = date;
+        }
     }
 }
