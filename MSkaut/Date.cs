08653f3de2a8d3e7d1e738fdca7198433322b78c
Ondrej Litschmann <xlitschm@fi.muni.cz>
2025-04-13 20:07:00 +0200

    add date class

---
+++ MSkaut/Date.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Date
+{
+	public ushort day { get; set; }
+    public ushort Month { get; set; }
+	public ushort year { get; set; }
+
+    public Date(ushort day, ushort month, ushort year)
+	{
+		this.day = day;
+		this.month
+		this.year = year;
+	}
+}
