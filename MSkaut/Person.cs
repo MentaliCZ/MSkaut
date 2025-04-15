197fa86d85ae644ac996b99c4ef7c35a640afa25
Ondrej Litschmann <xlitschm@fi.muni.cz>
2025-04-15 21:21:30 +0200

    add person class

---
+++ MSkaut/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MSkaut
+{
+	public class Person
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public Date BirthDate { get; set; }
+		public string Gender { get; set; }
+
+		public Person(string firstName, string lastName, Date birthDate, string gender)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.BirthDate = birthDate;
+			this.Gender = gender;
+		}
+	}
+}
