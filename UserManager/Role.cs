83ac742296f709871057bd477d45da339cb83cc8
Ondrej Litschmann <xlitschm@fi.muni.cz>
2025-04-17 19:30:39 +0200

    add role class

---
+++ UserManager/Role.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserManager
+{
+	public class Role
+	{
+		public string Name { get; private set; }
+		public string Description { get; private set; }
+		// hierarchy, larger number means more privileges
+		private int hierarchy;
+
+		public Role(string name, int hierarchy)
+		{
+			this.Name = name;
+			this.hierarchy = hierarchy;
+		}
+
+        public Role(string name, string description, int hierarchy) : this(name, hierarchy)
+        {
+			this.Description = description;
+        }
+    }
+}
