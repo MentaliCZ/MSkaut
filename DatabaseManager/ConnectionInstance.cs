085817080f6e3396b23825be13d95b1eb6414369
Ondrej Litschmann <xlitschm@fi.muni.cz>
2025-04-18 15:02:13 +0200

    add method to connect to db

--- DatabaseManager/ConnectionInstance.cs
+++ DatabaseManager/ConnectionInstance.cs
@@ -6,12 +6,18 @@ namespace DatabaseManager
     {
         private Client client;
 
-        public ConnectionInstance() {
-            initClient(); 
+        private ConnectionInstance() {
 
         }
 
-        private async void initClient() {
+        public static async Task<ConnectionInstance> CreateInstance() 
+        {
+            ConnectionInstance instance = new();
+            await instance.InitClient();
+            return instance;
+        }
+
+        public async Task InitClient() {
             string publicKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoZm55dWlxY3lkdXp1Ym92ZmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5NzA4MzcsImV4cCI6MjA2MDU0NjgzN30.vzBBxZxRRHa0HbAeG7mcK_4t-EbaJ7-rDHKvezw2zZ4";
             string urlString = "https://rhfnyuiqcyduzubovfig.supabase.co";
 
@@ -27,6 +33,8 @@ namespace DatabaseManager
             client = new Client(url, key, options);
             await client.InitializeAsync();
         }
+
+
 
     }
 }
