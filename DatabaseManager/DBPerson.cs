3d246b0d678bf2ad481aa011a46cdc92f987cc9a
Ondrej Litschmann <xlitschm@fi.muni.cz>
2025-04-21 19:21:38 +0200

    add class DBPerson

---
+++ DatabaseManager/DBPerson.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Supabase;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace DatabaseManager
+{
+    [Table("Person")]
+    public class DBPerson : BaseModel
+	{
+        [PrimaryKey("person_id")]
+        public int Id { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("birth_date")]
+        public DateOnly BirthDate { get; set; }
+
+        [Column("gender_id")]
+        public int GenderId { get; set; }
+
+        [Column("creator_id")]
+        public int CreatorId { get; set; }
+
+        public static async Task<DBPerson?> GetPerson(int id, Client client)
+        {
+            return await client
+           .From<DBPerson>()
+           .Select(x => new object[] { x.Id, x.FirstName, x.LastName, x.BirthDate, x.GenderId })
+           .Where(x => x.Id == id)
+           .Single();
+        }
+
+        public static async Task<bool> CreatePerson(string firstName, string lastName, DateOnly birthDate,
+                                                    int genderId, int creatorId, Client client)
+        {
+            var dbPerson = new DBPerson
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                BirthDate = birthDate,
+                GenderId = genderId,
+                CreatorId = creatorId
+            };
+
+            await client.From<DBPerson>().Insert(dbPerson);
+
+            return true;
+        }
+
+
+    }
+}
