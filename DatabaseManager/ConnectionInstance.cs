7e1cdad82d9fee011c3c291d6fa0e5b561c3adb1
Ondrej Litschmann <xlitschm@fi.muni.cz>
2025-06-17 20:16:26 +0200

    moved database info to text files

--- DatabaseManager/ConnectionInstance.cs
+++ DatabaseManager/ConnectionInstance.cs
@@ -5,6 +5,8 @@ namespace DatabaseManager
     public class ConnectionInstance
     {
         public Client Client { get; private set; }
+        private static string URL_FILE = "url.txt";
+        private static string PUBLIC_KEY_FILE = "key.txt";
 
         private ConnectionInstance() { }
 
@@ -17,9 +19,8 @@ namespace DatabaseManager
 
         private async Task InitClient()
         {
-            string publicKey =
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoZm55dWlxY3lkdXp1Ym92ZmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5NzA4MzcsImV4cCI6MjA2MDU0NjgzN30.vzBBxZxRRHa0HbAeG7mcK_4t-EbaJ7-rDHKvezw2zZ4";
-            string urlString = "https://rhfnyuiqcyduzubovfig.supabase.co";
+            string publicKey = File.ReadAllText(PUBLIC_KEY_FILE);
+            string urlString = File.ReadAllText(URL_FILE);
 
             Client = new Client(urlString, publicKey);
             await Client.InitializeAsync();
